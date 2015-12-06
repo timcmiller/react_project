cb221c49e89127512ae88cd3048a6e21330ed12a
timcmiller <timcmiller@live.com>
2015-12-05 16:30:38 -0800

    cleanup of file structure

--- models/user.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-
-var userSchema = new mongoose.Schema({
-  username: {type: String, require: true, index: true, unique: true}
-});
-
-module.exports = exports = mongoose.model('User', userSchema);
--- public/js/entry.js
+++
@@ -1,2 +0,0 @@
-require(__dirname + '/modules/modules.js');
-
